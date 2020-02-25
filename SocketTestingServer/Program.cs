4e4acc71b417deb5044d67e9598ff0940775c3f8
Nathan Solomon <nbsolomon4559@gmail.com>
2020-02-24 19:27:25 -0600

    initial repo setup

---
+++ SocketTestingServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SocketTestingServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
