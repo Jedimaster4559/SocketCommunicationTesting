97614714aa7a8f3c87295097a8be0f31fed92e7a
Nathan Solomon <nbsolomon4559@gmail.com>
2020-02-24 23:09:05 -0600

    fixed issue with abstracting off of Socket

--- SocketTestingServer/ClientSocket.cs
+++ SocketTestingServer/ClientSocket.cs
@@ -3,7 +3,18 @@ using System.Net;
 using System.Net.Sockets;
 
 namespace SocketTestingServer {
-    class ClientSocket : Socket {
-        
+    class ClientSocket : Socket
+    {
+        public ClientSocket(SocketInformation socketInformation) : base(socketInformation)
+        {
+        }
+
+        public ClientSocket(SocketType socketType, ProtocolType protocolType) : base(socketType, protocolType)
+        {
+        }
+
+        public ClientSocket(AddressFamily addressFamily, SocketType socketType, ProtocolType protocolType) : base(addressFamily, socketType, protocolType)
+        {
+        }
     }
 }
